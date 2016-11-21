3578c086bc0ea0747f871af492ea7d08bf6b4ba4
wanek96 <wanek96@mail.ru>
2016-11-21 11:16:23 +0200

    start

--- OSHomeWork/Form1.Designer.cs
+++ OSHomeWork/Form1.Designer.cs
@@ -28,12 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 47);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(221, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Разрешить запуск любых приложений";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);            
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(563, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";           
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(942, 481);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- OSHomeWork/Form1.cs
+++ OSHomeWork/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Win32;
 
 namespace OSHomeWork
 {
@@ -15,6 +16,26 @@ namespace OSHomeWork
         public Form1()
         {
             InitializeComponent();
+            registryKey = Registry.CurrentConfig;
         }
+        private readonly RegistryKey registryKey;
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                label1.Text = String.Empty;
+                foreach (var item in Registry.CurrentUser.GetSubKeyNames())
+                {
+                    label1.Text = label1.Text + "\n " + item;
+                }
+            }
+            else
+            {
+                label1.Text = registryKey.Name;
+            }
+        }
+
+
     }
 }
