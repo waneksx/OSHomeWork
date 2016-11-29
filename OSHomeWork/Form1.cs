3ae4f9cab41c7bd55c4459d5d04207f16e52dbc3
wanek96 <wanek96@mail.ru>
2016-11-29 20:42:23 +0200

    need commit

--- OSHomeWork/Form1.cs
+++ OSHomeWork/Form1.cs
@@ -16,23 +16,26 @@ namespace OSHomeWork
         public Form1()
         {
             InitializeComponent();
-            registryKey = Registry.CurrentConfig;
         }
-        private readonly RegistryKey registryKey;
 
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
+            var path = Registry.CurrentUser.OpenSubKey("Software").OpenSubKey("Microsoft").OpenSubKey("Windows").OpenSubKey("CurrentVersion").OpenSubKey("Policies").OpenSubKey("Explorer", true);
             if (checkBox1.Checked)
             {
-                label1.Text = String.Empty;
-                foreach (var item in Registry.CurrentUser.GetSubKeyNames())
-                {
-                    label1.Text = label1.Text + "\n " + item;
-                }
+                path.SetValue("RestrictRun", 0);
+                path.SetValue("DisallowRun", 1);
+                path.CreateSubKey("DisallowRun", true);
+                var path2 = path.OpenSubKey("DisallowRun", true);
+                path2.SetValue("1", "calc.exe");                
             }
             else
             {
-                label1.Text = registryKey.Name;
+                path.SetValue("DisallowRun", 0);
+                path.SetValue("RestrictRun", 1);
+                path.CreateSubKey("RestrictRun", true);
+                var path2 = path.OpenSubKey("RestrictRun");
+                path2.SetValue("1","regedit.exe");
             }
         }
 
