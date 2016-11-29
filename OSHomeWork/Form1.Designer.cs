585ed7ab1d500a7d94a88f65d3931e10af1d65e4
wanek96 <wanek96@mail.ru>
2016-11-29 22:44:29 +0200

    maybe end

--- OSHomeWork/Form1.Designer.cs
+++ OSHomeWork/Form1.Designer.cs
@@ -30,37 +30,72 @@
         {
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(12, 47);
+            this.checkBox1.Location = new System.Drawing.Point(136, 47);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(221, 17);
+            this.checkBox1.Size = new System.Drawing.Size(278, 17);
             this.checkBox1.TabIndex = 0;
-            this.checkBox1.Text = "Разрешить запуск любых приложений";
+            this.checkBox1.Text = "Разрешить\\запретить запуск любых приложений";
             this.checkBox1.UseVisualStyleBackColor = true;
-            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);            
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(563, 51);
+            this.label1.Location = new System.Drawing.Point(531, 48);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(0, 13);
             this.label1.TabIndex = 1;
-            this.label1.Text = "label1";           
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(451, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 49);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Отменить все измененя";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(144, 194);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(122, 81);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Старт дефрагментации";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(451, 198);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(119, 72);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Начать проверку диска";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(942, 481);
+            this.ClientSize = new System.Drawing.Size(643, 332);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.checkBox1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Домашнее задание (Вариант 10)";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -70,6 +105,9 @@
 
         private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- OSHomeWork/Form1.cs
+++ OSHomeWork/Form1.cs
@@ -8,6 +8,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Microsoft.Win32;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
 
 namespace OSHomeWork
 {
@@ -18,27 +21,65 @@ namespace OSHomeWork
             InitializeComponent();
         }
 
+        static RegistryKey pathForChange = Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer", true);
+
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
-            var path = Registry.CurrentUser.OpenSubKey("Software").OpenSubKey("Microsoft").OpenSubKey("Windows").OpenSubKey("CurrentVersion").OpenSubKey("Policies").OpenSubKey("Explorer", true);
+            var path = pathForChange;
             if (checkBox1.Checked)
             {
                 path.SetValue("RestrictRun", 0);
                 path.SetValue("DisallowRun", 1);
                 path.CreateSubKey("DisallowRun", true);
                 var path2 = path.OpenSubKey("DisallowRun", true);
-                path2.SetValue("1", "calc.exe");                
+                path2.SetValue("1", "calc.exe");
             }
             else
             {
                 path.SetValue("DisallowRun", 0);
                 path.SetValue("RestrictRun", 1);
                 path.CreateSubKey("RestrictRun", true);
-                var path2 = path.OpenSubKey("RestrictRun");
+                var path2 = path.OpenSubKey("RestrictRun", true);
                 path2.SetValue("1","regedit.exe");
+                path2.SetValue("2", "OSHomeWork.exe");
             }
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var path = pathForChange;
+            if (path.GetSubKeyNames().Where(x => x == "RestrictRun").FirstOrDefault()!= null)
+                path.DeleteSubKey("RestrictRun");
+            if (path.GetSubKeyNames().Where(x => x == "DisallowRun").FirstOrDefault() != null)
+                path.DeleteSubKey("DisallowRun");
+            var path2 = path;
+            if (path2.GetValue("RestrictRun") != null)
+             path2.DeleteValue("RestrictRun");
+            if (path2.GetValue("DisallowRun") != null)
+                path2.DeleteValue("DisallowRun");
+        }
 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //to check - run Services.msc
+            ProcessStartInfo pf = new ProcessStartInfo(Path.Combine(Environment.SystemDirectory, "dfrgui.exe"));
+            pf.Verb = "runas";
+            Process.Start(pf);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+
+            process.StartInfo.CreateNoWindow = false;
+            process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            process.StartInfo.FileName = "ChkDsk.exe";
+            process.StartInfo.Arguments = string.Concat("e:", " /f");
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardInput = true;
+
+            process.Start();
+        }
     }
 }
